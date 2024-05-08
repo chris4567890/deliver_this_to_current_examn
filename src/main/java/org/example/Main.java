01428943b8f6fb2f6619172f81e80e895e8d9e51
chris4567890 <starchristian4@hotmail.com>
2024-05-08 09:54:57 +0200

    måske hente jorg
    måske tag fat i jorg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
         boolean isTesting = false;
-        ApplicationConfig app = ApplicationConfig.getInstance().initiateServer().setExceptionHandling().startServer(7008).setRoutes(Routes.getRoutes(isTesting));
+        ApplicationConfig app = ApplicationConfig.getInstance().initiateServer().setExceptionHandling().startServer(7070).setRoutes(Routes.getRoutes(isTesting));
     }
 }
