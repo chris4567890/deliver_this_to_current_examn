ebd477e1d8d051db300289d0cd1032a60a999d66
chris4567890 <starchristian4@hotmail.com>
2024-05-09 19:18:22 +0200

    update the stuf
    the stuff is the stuff.

--- src/main/java/org/controllers/SecurityController.java
+++ src/main/java/org/controllers/SecurityController.java
@@ -10,10 +10,11 @@ public class SecurityController {
     public Handler authorization() {
 
         return (ctx->{
-            ctx.json("hello I am present here in authorization");
-            TokenDTO tokenDTO =new TokenDTO(ctx.header("username"), ctx.header("Authorization").split(" ")[1]);
-            TokenUtil.tokenIsValid(tokenDTO.getToken(),"841D8A6C80CBA4FCAD32D5367C18C53B");
-
+             ctx.status(200);
+             return;
+            //TokenDTO tokenDTO =new TokenDTO(ctx.header("username"), ctx.header("Authorization").split(" ")[1]);
+            //TokenUtil.tokenIsValid(tokenDTO.getToken(),"841D8A6C80CBA4FCAD32D5367C18C53B");
+            //TODO: fix when we find out how to find route roles
         });
 
     }
