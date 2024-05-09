ee45cf4f211e434f775bf0dbd2f5db4d48580d0c
chris4567890 <starchristian4@hotmail.com>
2024-05-09 19:03:01 +0200

    update security
    added small test update

--- src/main/java/org/controllers/SecurityController.java
+++ src/main/java/org/controllers/SecurityController.java
@@ -10,6 +10,7 @@ public class SecurityController {
     public Handler authorization() {
 
         return (ctx->{
+            ctx.json("hello I am present here in authorization");
             TokenDTO tokenDTO =new TokenDTO(ctx.header("username"), ctx.header("Authorization").split(" ")[1]);
             TokenUtil.tokenIsValid(tokenDTO.getToken(),"841D8A6C80CBA4FCAD32D5367C18C53B");
 
